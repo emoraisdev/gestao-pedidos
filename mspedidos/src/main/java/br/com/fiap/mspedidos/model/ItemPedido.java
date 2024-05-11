c34f52e48f633adeba79e7302b675549838a62fd
Tiago Germano <tiagogermanos@gmail.com>
2024-05-11 02:38:57 -0300

    mapeamento das entidade do banco

--- mspedidos/src/main/java/br/com/fiap/mspedidos/model/ItemPedido.java
+++ mspedidos/src/main/java/br/com/fiap/mspedidos/model/ItemPedido.java
@@ -1,14 +1,29 @@
 package br.com.fiap.mspedidos.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Entity
 public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @Column(nullable = false)
     private Long produtoId;
+
+    @Column(nullable = false)
     private Integer quantidade;
-    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private BigDecimal valorUnitario;
 
 }
--- mspedidos/src/main/java/br/com/fiap/mspedidos/model/Pedido.java
+++ mspedidos/src/main/java/br/com/fiap/mspedidos/model/Pedido.java
@@ -1,15 +1,29 @@
 package br.com.fiap.mspedidos.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Calendar;
 import java.util.List;
 
 @Data
+@Entity
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private Long clientId;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemPedido> itensPedido;
+
+    @Column(nullable = false)
     private Calendar dataPedido;
+
+    @Column(nullable = false)
     private StatusPedido status;
 }
