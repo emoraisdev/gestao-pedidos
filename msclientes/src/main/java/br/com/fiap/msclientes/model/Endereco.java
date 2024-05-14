b2fb1ce0a4aefd18500c954f0787a706baac1f6d
Victor <victorvalencio26@gmail.com>
2024-05-14 19:26:28 -0300

    add cliente

--- msclientes/src/main/java/br/com/fiap/msclientes/model/Endereco.java
+++ msclientes/src/main/java/br/com/fiap/msclientes/model/Endereco.java
@@ -2,20 +2,35 @@ package br.com.fiap.msclientes.model;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 @Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Endereco {
 
     @NotBlank(message = "A rua é obrigatória")
     private String rua;
+
+    @NotBlank(message = "O número é obrigatório")
+    private String numero;
+
+    @NotBlank(message = "O bairro é obrigatório")
+    private String bairro;
 
     @NotBlank(message = "A cidade é obrigatória")
     private String cidade;
 
     @NotBlank(message = "O estado é obrigatório")
     private String estado;
+
+    @NotBlank(message = "O país é obrigatório")
+    private String pais;
 
     @NotBlank(message = "O CEP é obrigatório")
     private String cep;
