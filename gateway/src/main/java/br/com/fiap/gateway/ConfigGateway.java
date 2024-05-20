cdf7188b8b2437fbd33acf8fe8dc834d28305ad2
emoraisdev <evertonsmorais@gmail.com>
2024-05-20 19:51:01 -0300

    Ajustes gateway

--- gateway/src/main/java/br/com/fiap/gateway/ConfigGateway.java
+++ gateway/src/main/java/br/com/fiap/gateway/ConfigGateway.java
@@ -1,5 +1,6 @@
 package br.com.fiap.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +9,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConfigGateway {
 
+    @Value("${api.mslogistica.server}")
+    private String mslogistica;
+
+    @Value("${api.msclientes.server}")
+    private String msclientes;
+
+    @Value("${api.msprodutos.server}")
+    private String msprodutos;
+
+    @Value("${api.mscargaprodutos.server}")
+    private String mscargaprodutos;
+
+    @Value("${api.mspedidos.server}")
+    private String mspedidos;
+
     @Bean
     public RouteLocator custom(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("logistica", r -> r.path("/logistica/**")
                         .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8085"))
+                        .uri(mslogistica))
                 .route("cliente", r -> r.path("/cliente/**")
                         .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8088"))
+                        .uri(msclientes))
+                .route("produtos", r -> r.path("/produtos/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri(msprodutos))
+                .route("cargaprodutos", r -> r.path("/cargaprodutos/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri(mscargaprodutos))
+                .route("pedidos", r -> r.path("/pedidos/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri(mspedidos))
                 .build();
 
     }
